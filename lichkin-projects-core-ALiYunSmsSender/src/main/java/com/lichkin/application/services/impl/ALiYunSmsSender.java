2521636ed9634d37eaf78d8e253e18aa1ca68b69
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-13 20:05:03 +0800

    阿里云短信配置从文件中读取

--- lichkin-projects-core-ALiYunSmsSender/src/main/java/com/lichkin/application/services/impl/ALiYunSmsSender.java
+++ lichkin-projects-core-ALiYunSmsSender/src/main/java/com/lichkin/application/services/impl/ALiYunSmsSender.java
@@ -1,6 +1,7 @@
 package com.lichkin.application.services.impl;
 
 import java.util.Map;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import com.lichkin.framework.json.LKJsonUtils;
 import com.lichkin.framework.log.LKLog;
 import com.lichkin.framework.log.LKLogFactory;
 import com.lichkin.framework.utils.i18n.LKI18NUtils;
+import com.lichkin.framework.utils.security.properties.LKPropertiesReader;
 
 /**
  * 阿里云短信发送服务类
@@ -32,24 +34,21 @@ public class ALiYunSmsSender implements LKSmsSender {
 	/** 云通信产品域名 */
 	private static final String domain = "dysmsapi.aliyuncs.com";
 
-	@Value("${com.lichkin.api.smsSender.aliyun.accessKeyId}")
-	private String accessKeyId;
-
-	@Value("${com.lichkin.api.smsSender.aliyun.accessKeySecret}")
-	private String accessKeySecret;
-
-	@Value("${com.lichkin.api.smsSender.aliyun.signName}")
-	private String signName;
+	@Value("${com.lichkin.api.smsSender.aliyun.filePath:/opt/security/aliyun/smsSender.properties}")
+	private String fileName;
 
 
 	@Override
 	public void send(String locale, String busType, String subBusType, String cellphone, String smsContent, Map<String, Object> replaceParams) {
+		// 从文件中读取配置信息
+		Properties configs = LKPropertiesReader.read(fileName);
+
 		// 可自助调整超时时间
 		System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
 		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
 
 		// 初始化acsClient,暂不支持region化
-		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", configs.getProperty("accessKeyId"), configs.getProperty("accessKeySecret"));
 		try {
 			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
 		} catch (ClientException e) {
@@ -63,7 +62,7 @@ public class ALiYunSmsSender implements LKSmsSender {
 		// 必填:待发送手机号
 		request.setPhoneNumbers(cellphone);
 		// 必填:短信签名-可在短信控制台中找到
-		request.setSignName(signName);
+		request.setSignName(configs.getProperty("signName"));
 		// 必填:短信模板-可在短信控制台中找到
 		request.setTemplateCode(LKI18NUtils.getString(LKI18NUtils.getLocale(locale), "api", "com.lichkin.api.sms." + busType + "." + subBusType + ".aliyun.templateCode"));
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
