c1d6ce193d72c52d289c219792659e7df024f3c8
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-13 15:57:16 +0800

    账号登录接口

---
+++ lichkin-projects-core-user/src/main/java/com/lichkin/application/apis/AccountLogin/Statics.java
@@ -0,0 +1,17 @@
+package com.lichkin.application.apis.AccountLogin;
+
+/**
+ * 账号登录
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+class Statics {
+
+	private static final String NAME = "AccountLogin";
+
+	static final String SUB_URL = "/" + NAME;
+
+	static final String CONTROLLER_NAME = NAME + "C";
+
+	static final String SERVICE_NAME = NAME + "S";
+
+}
