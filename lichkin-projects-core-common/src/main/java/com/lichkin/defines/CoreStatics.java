54336c3d69c4fda63eb6d622b62b1f56986600d4
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-13 14:51:16 +0800

    常量定义整理

--- lichkin-projects-core-common/src/main/java/com/lichkin/defines/CoreStatics.java
+++ lichkin-projects-core-common/src/main/java/com/lichkin/defines/CoreStatics.java
@@ -8,18 +8,13 @@ import com.lichkin.framework.defines.LKFrameworkStatics;
  */
 public interface CoreStatics {
 
-	public static final String USER_SSO_URL = "/User/SSO" + LKFrameworkStatics.WEB_MAPPING_PAGES + "?redirectTo=";
+	/** 用户单点登录地址 */
+	public static final String USER_SSO_URL = "/user/SSO" + LKFrameworkStatics.WEB_MAPPING_PAGES + "?redirectUrl=";
 
-	public static final String USER_EMPLOYEE_SSO_URL = "/UserEmployee/SSO" + LKFrameworkStatics.WEB_MAPPING_PAGES + "?redirectTo=";
+	/** 员工单点登录地址 */
+	public static final String EMPLOYEE_SSO_URL = "/employee/SSO" + LKFrameworkStatics.WEB_MAPPING_PAGES + "?redirectUrl=";
 
-	public static final String EMPLOYEE_SSO_URL = "/Employee/SSO" + LKFrameworkStatics.WEB_MAPPING_PAGES + "?redirectTo=";
-
-	public static final String SECURITY_CENTER_URL = "/appSecurityCenter/index" + LKFrameworkStatics.WEB_MAPPING_PAGES;
-
-	public static final String ACTIVITI_CENTER_URL = "/activitiCenter/index" + LKFrameworkStatics.WEB_MAPPING_PAGES;
-
-	public static final String APP_BANNER_URL = "/appBanner/detail" + LKFrameworkStatics.WEB_MAPPING_PAGES;
-
-	public static final String APP_NEWS_URL = "/appNews/detail" + LKFrameworkStatics.WEB_MAPPING_PAGES;
+	/** 安全中心地址 */
+	public static final String SECURITY_CENTER_URL = USER_SSO_URL + "/securityCenter/index" + LKFrameworkStatics.WEB_MAPPING_PAGES;
 
 }
