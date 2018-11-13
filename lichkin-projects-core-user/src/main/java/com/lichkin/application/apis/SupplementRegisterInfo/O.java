134f1c51faca8003091b13bb2fe0ee200b670014
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-13 16:10:00 +0800

    完善注册信息接口

---
+++ lichkin-projects-core-user/src/main/java/com/lichkin/application/apis/SupplementRegisterInfo/O.java
@@ -0,0 +1,15 @@
+package com.lichkin.application.apis.SupplementRegisterInfo;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class O {
+
+	/** 登录名 */
+	private final String loginName;
+
+}
