9cb2abe5d6036305d0cde7c79d0186e7cc6eff72
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-07 03:34:20 +0800

    增加法定节假日类型枚举

---
+++ lichkin-projects-core-common/src/main/java/com/lichkin/framework/defines/enums/impl/HolidayTypeEnum.java
@@ -0,0 +1,25 @@
+package com.lichkin.framework.defines.enums.impl;
+
+import com.lichkin.framework.defines.enums.LKEnum;
+
+/**
+ * 法定节假日类型枚举
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+public enum HolidayTypeEnum implements LKEnum {
+
+	/** 法定节假日 */
+	H,
+
+	/** 周末 */
+	W,
+
+	/** 调休 */
+	R,
+
+	/** 补班 */
+	S,
+
+	;
+
+}
